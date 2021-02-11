3bb761d1793c5ab1f78420ce0085d61a5aa84b88
Adukkadukkam <harikumar.adukkadukkam@sap.com>
2021-02-11 16:25:10 +0530

    Completed module

--- src/main/java/io/jzheaux/springsecurity/resolutions/User.java
+++ src/main/java/io/jzheaux/springsecurity/resolutions/User.java
@@ -1,6 +1,50 @@
 package io.jzheaux.springsecurity.resolutions;
 
-import java.io.Serializable;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+import lombok.RequiredArgsConstructor;
 
+import javax.persistence.*;
+import java.io.Serializable;
+import java.util.ArrayList;
+import java.util.Collection;
+import java.util.UUID;
+
+@Entity(name="users")
+@Data
+@NoArgsConstructor
 public class User implements Serializable {
+
+    @Id
+    UUID id=UUID.randomUUID();
+
+    @Column
+    String username;
+
+    @Column
+    String password;
+
+    @Column
+    boolean enabled = true;
+
+    @OneToMany(fetch= FetchType.EAGER, cascade=CascadeType.ALL)
+    Collection<UserAuthority> userAuthorities = new ArrayList<>();
+
+    public User(String username, String password) {
+        this.username = username;
+        this.password = password;
+    }
+
+    public void grantAuthority(String authority) {
+        UserAuthority userAuthority = new UserAuthority(this, authority);
+        this.userAuthorities.add(userAuthority);
+    }
+
+    public User(User user) {
+        this.id = user.id;
+        this.username = user.username;
+        this.password = user.password;
+        this.enabled = user.enabled;
+        this.userAuthorities = user.userAuthorities;
+    }
 }
--- src/main/java/io/jzheaux/springsecurity/resolutions/UserAuthority.java
+++ src/main/java/io/jzheaux/springsecurity/resolutions/UserAuthority.java
@@ -1,4 +1,28 @@
 package io.jzheaux.springsecurity.resolutions;
 
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+import javax.persistence.*;
+import java.util.UUID;
+
+@Entity(name="authorities")
+@Data
+@NoArgsConstructor
 public class UserAuthority {
+    @Id
+    UUID id;
+
+    @Column
+    String authority;
+
+    @JoinColumn(name="username", referencedColumnName="username")
+    @ManyToOne
+    User user;
+
+    public UserAuthority(User user, String authority) {
+        this.id = UUID.randomUUID();
+        this.user = user;
+        this.authority = authority;
+    }
 }
